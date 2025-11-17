e883de4253bd2801531413385dfd04c1ee673fd8
HELLEN <hellen.castelan@escola.pr.gov.br>
2025-11-17 07:45:29 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,1 +1,36 @@
+criaCartao(
+    'Futebol',
+    'Qual seleção tem mais copas do mundo?',
+    'Brasil'
+)
+
+criaCartao(
+    'Categoria',
+    'Pergunta',
+    'Resposta'
+)
+
+criaCartao(
+    'Categoria',
+    'Pergunta',
+    'Resposta'
+)
+
+criaCartao(
+    'fama',
+    'quem e mais famosa, riana ou beyonce',
+    'beyonce'
+)
+
+criaCartao(
+    'quem',
+    'a mulher tem na frente, o homem tem atras e o papa nao tem',
+    'M'
+)
+
+criaCartao(
+    'musica',
+    'qual  a musica mais famosa do michael jackson',
+    'billie jean'
+)
 
